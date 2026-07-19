1525a5ef2a868a7b357adc5c825eb81a804fe00e
ebrahimhaddad <blacksmith80@gmail.com>
2026-07-19 13:55:12 +0330

    fix: add graceful shutdown for SIGTERM

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,15 @@ pool
   .then(() => console.log("MySQL connected successfully"))
   .catch((err) => console.error("MySQL connection failed:", err));
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   logger.info(`Server running on http://localhost:${PORT}`);
+});
+
+// Graceful shutdown in sake of Railway
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received — shutting down gracefully");
+  server.close(() => {
+    pool.end();
+    logger.info("Process terminated");
+  });
 });
